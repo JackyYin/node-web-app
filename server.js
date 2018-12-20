015dd1129d833385e563823e1c5fc51dfaf6b57a
殷豪 <jjyyg1123@gmail.com>
2018-12-20 17:00:31 +0800

    feat: 完成mongodb連線

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose')
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+
+mongoose.connect('mongodb://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@' + process.env.DB_HOST + '/' + process.env.DB_NAME + '?authSource=admin' );
 
 // App
 const app = express();
